349533aa97b33df37606539d0055c5dbbfb768e3
juliocpmelo <julio.melo@imd.ufrn.br>
2021-07-15 11:31:42 -0300

    Adicionando correções e feedback

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,23 @@
 #include "KenoBet.hpp"
 #include "Interface.hpp"
 #include "Operations.hpp"
+/*
+Avaliação
+Processamento do Arquivo de Entrada: 0,75 (faltou processar o caso em que o arquivo tem menos de 3 linhas)
+Execução correta: 1
+Interfcace Textual: 1
+Documentação: 1
+Compilação automatizada: 1
+Organização em src, include, build, data: 1
+
+Comentários
+- Acho que a organização ficou muito boa, para melhorar vocês poderiam adicionar alguns atributos na classe Interface
+especialmente uma referencia para KenoBet.
+- A parte de processamento do arquivo na classe Arquive também poderia ficar mais focada dentro da classe e não em funções
+separadas. Veja que quando vocês enviam uma referencia de Arquive na função readlines, fica bem claro que essa função poderia
+ser um método da classe arquive. Como as demais funções não se encaixam como métodos (pois são auxiliares de arquive), não tem
+problema colocar elas separadas, mas vocês poderiam coloca-las como membros estáticos da classe arquive melhorando ainda mais a organização.
+*/
 
 int main(int argc, char *argv[]) {
     Interface interface;
